c7330716829e2c59a46f2fcf406235064e346614
Tamura <tamurafelipe@gmail.com>
2025-02-24 16:05:33 -0300

    ajustes responsivo

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -11,12 +11,12 @@ const Card = ({image, name, description, assessment, avaliation_quantity, price}
         <div className="p-0.5 xl:p-2.5">
             <h2 className="text-black text-title xl:text-title-desktop font-bold mb-0.5">{name}</h2>
             <p className="text-description xl:text-description-desktop mb-0.5 font-medium">{description}</p>
-            <div className="flex items-center gap-0.5 mb-0.5">
+            <div className="flex flex-col md:flex-row items-center gap-0.5 mb-0.5">
                 <StarRating rating={assessment} /> 
                 <span className="text-description xl:text-description-desktop text-neutral-500">({avaliation_quantity} avaliações)</span>
             </div>
-            <div className="flex items-center justify-between mb-0.5">
-                <p className="font-bold text-[22px] xl:text-[40px] text-sky-500">R$ {price.toFixed(2)}</p>
+            <div className="flex flex-col md:flex-row items-center justify-between mb-0.5">
+                <p className="font-bold text-price xl:text-price-desktop text-sky-500">R$ {price.toFixed(2)}</p>
                 <Button>Comprar agora</Button>
             </div>
             <div className="flex items-center gap-0.5">
